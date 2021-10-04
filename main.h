93c6e5dd6ca4f656e3097ea344f55a5bd040d971
YourDevLebo <djlebzana@gmail.com>
2021-10-04 10:53:19 -0400

    added header file

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* needed header files for printf */
+#include <stdard.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* given prototype for printf */
+int _printf(const char *format, ...);
+
+/* external prototype functions */
+
+#endif
